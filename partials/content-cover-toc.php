f2deb9b2787f6e5df66fa8e9a5a91b253a446c78
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-08-27 16:23:14 +0200

    location of 'Show All' buton in cover page changed

---
+++ partials/content-cover-toc.php
@@ -0,0 +1,19 @@
+<section class="block block-toc">
+	<?php if ( pb_get_first_post_id() ) : ?>
+		<h2 class="block__title block-toc__title"><?php _e( 'Contents', 'pressbooks-book' ); ?></h2>
+		<!-- CHANGED: toc__toggle originally is afet <nav> element-->
+		<div class="toc__toggle" aria-expanded="false">
+			<button id="show" class="button"><?php _e( 'Show All Contents' ); ?></button>
+			<button id="hide" class="button"><?php _e( 'Hide All Contents' ); ?></button>
+		</div>
+		<nav><?php include( locate_template( 'partials/content-toc.php' ) ); ?></nav>
+	<?php
+	endif;
+	/**
+	 * Append content to cover table of contents block.
+	 *
+	 * @since 2.0.0
+	 */
+	do_action( 'pb_book_cover_after_toc' );
+	?>
+</section>
