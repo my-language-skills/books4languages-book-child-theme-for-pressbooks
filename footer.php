2a6da606f59a82f76185024d1fdf8858174d9994
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-09-03 10:27:07 +0200

    typo

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@ echo $multipage ? ' footer--multipage' : '';
 		<section class="footer__pressbooks">
 			<a class="footer__pressbooks__icon" href="https://books4languages.com/" title="Books For Languages">
 				<?php // TODO ?>
-				<img src="/wp-content/themes/books4languages-book-child-theme-for-pressbooks/assets/images/books4languages-icon.png" alt="Books4languages Logo">
+				<img src="/wp-content/themes/books4languages-book-child-theme-for-pressbooks/assets/images/books4languages-icon.png" alt="Books4Languages Logo">
 			</a>
 			<div class="footer__pressbooks__links">
 				<?php
