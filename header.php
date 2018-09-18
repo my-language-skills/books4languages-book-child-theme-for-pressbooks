d8e7ad93cd1f7b8a3fd319442568fa388ae2ae1b
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-09-18 10:07:05 +0200

    links to registration and subscription details

--- header.php
+++ header.php
@@ -78,6 +78,9 @@ if ( \Pressbooks\Book\Helpers\social_media_enabled() ) {
 				<nav class="js-header-nav" id="navigation">
 					<ul id="nav-primary-menu" class="nav--primary">
 						<?php echo \Pressbooks\Book\Helpers\display_menu(); ?>
+						<?php if (! is_user_logged_in()):?>
+						| <li><a href="./register/">Sign Up</a></li>
+						<?php endif; ?>
 					</ul>
 				</nav>
 			</div>
