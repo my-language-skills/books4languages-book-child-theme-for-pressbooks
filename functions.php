ea9e38fcb3f3e2bfa7d2d6c39a96e3e666738bf7
Antonio Devís <colomett@gmail.com>
2019-11-27 17:53:24 +0100

    Remove external Google Fonts

--- functions.php
+++ functions.php
@@ -62,3 +62,34 @@ function pbc_get_tablecontents_url(){
 	echo get_permalink($postID = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_name = 'table-of-contents'" ));
 }
 /** End of functionality*/
+
+/**
+ * Remove External Google Fonts
+ *
+ * SINCE v1.4.4
+ */
+/*
+fonts.gstatic.com
+fonts.googleapis.com
+*/
+/*
+function remove_google_fonts_stylesheet() {
+		wp_dequeue_style('Inconsolata');
+		wp_deregister_style('Inconsolata');
+}
+add_action( 'wp_enqueue_scripts', 'remove_google_fonts_stylesheet', 100 );
+*/
+
+if ( is_user_logged_in() ) {
+   // your code for logged in user
+} else {
+   // your code for logged out user
+	 add_filter( 'style_loader_src', function($href){
+	 if(strpos($href, "//fonts.googleapis.com/") === false) {
+	 return $href;
+	 }
+	 return false;
+	 });
+
+}
+/** End of functionality*/
