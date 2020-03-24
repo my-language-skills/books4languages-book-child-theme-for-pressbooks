02b8d1e9c83e88a0b693fec881cd63a46657f82f
Antonio D <colomett@gmail.com>
2020-03-24 09:05:57 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 <p><?php esc_html_e( 'It looks like nothing was found at this location. Try a search?', 'pressbooks-book' ); ?></p>
 <?php get_search_form(); ?>
 
-<p><img style="display: block; margin-left: auto; margin-right: auto;" src="https://media.giphy.com/media/M9ZpE3vcxH6bKuhL8b/giphy.gif" alt="Typewriter (GIF) designed by Georgy Pashkov." width="800" height="450" />Typewriter (GIF) designed by Georgy Pashkov</p>
+<p><img style="display: block; margin-left: auto; margin-right: auto;" src="https://media.giphy.com/media/hSdbwiVGiz72x67VyG/giphy.gif" alt="Typewriter (GIF) designed by Georgy Pashkov." width="800" height="450" />Typewriter (GIF) designed by Georgy Pashkov</p>
 <br>
 <h4>Are you searching for our index?</h4>
 <a class="button" href="<?php pbc_get_tablecontents_url() ?>"><?php _e( 'Site index', 'pressbooks-book' ); ?></a>
