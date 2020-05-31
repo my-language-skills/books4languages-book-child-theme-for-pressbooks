567453ddd3fb4b04b4da1b27df9fa5911ffed797
Colomet <colomet@hotmail.com>
2020-05-31 18:40:51 +0200

    downgrade

--- footer.php
+++ footer.php
@@ -5,53 +5,7 @@
 <?php
 global $multipage;
 ?>
-<!-- Books4Languages specifical footer -->
-<!-- https://stackoverflow.com/questions/38212961/how-do-i-create-a-three-column-responsive-footer -->
-<?php if ( !wp_is_mobile() && is_single()) { ?>
-	<div id="footerwrap">
-        <div id="footer-discover">
-          <h2>Discover Books4Languages</h2>
-					<p>Welcome to a place where language matter. On Books4Languages you can find a great collection of Open TextBooks. Read <a href="https://open.books4languages.com/"><?php _e( 'more', 'pressbooks-book' ); ?></a>.</p>
-        </div>
-        <div id="footer-contribute">
-					<h2>Make Books4Languages yours</h2>
-					<p>Translate the content or make an adaptation. If you already are a teacher, why not to be the author of your textbook? Read <a href="https://books4languages.com/contribute/"><?php _e( 'more', 'pressbooks-book' ); ?></a>.</p>
-        </div>
-        <div id="footer-membership">
-					<h2>Become a member</h2>
-					<p>Get unlimited access to the best language conten on Books4Languages — and support OpenEducation while you’re at it. Read <a href="https://open.books4languages.com/register/your-membership/"><?php _e( 'more', 'pressbooks-book' ); ?></a>.</p>
-				 </div>
-    		<div class="clear"></div>
-    </div>
-	<?php
-	}
-	?>
-<style>
-#footerwrap{
-    width: 100%;
-    position:relative;
-}
-#footer-discover, #footer-contribute, #footer-membership{
-  width:30%;
-  margin:0 20px 0 0;
-  display:inline-block;
-  vertical-align:top;
 
-}
-#footerwrap h2{
-	color: var(--primary);
-	font-size: 1.25rem;
-}
-#footerwrap  p{
-	margin-left: 2rem;
-	color: var(--body-text);
-}
-@media (max-width: 750px){
-  #footer-discover, #footer-contribute, #footer-membership{
-    width:100%;
-    margin:0;
-  }
-}</style>
 <!--
 -				ADDED: Add the h5p javascript.
 -
@@ -62,7 +16,6 @@ global $multipage;
 
 -->
 <script src="https://worksheet.books4languages.com/english/wp-content/plugins/h5p/h5p-php-library/js/h5p-resizer.js" charset="UTF-8"></script>
-
 
 <!--
 -				ADDED: Add a #copyright notice to the clipboard when someone copies a text from the website.
